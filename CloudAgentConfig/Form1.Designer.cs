4a8e3c2b7f6b42badf4606bde06590d504ab2766
张欢 <maskzh@qq.com>
2015-08-14 17:23:11 +0800

    Version1.1

--- CloudAgentConfig/Form1.Designer.cs
+++ CloudAgentConfig/Form1.Designer.cs
@@ -253,7 +253,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.ClientSize = new System.Drawing.Size(375, 292);
+            this.ClientSize = new System.Drawing.Size(372, 271);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
